61e697b0f8217494d357aad484e1cf4f13421972
adriperezz <127222542+adriperezz@users.noreply.github.com>
2024-04-23 09:57:24 +0200

    Changes storing photos

--- realEstate/models.py
+++ realEstate/models.py
@@ -1,4 +1,5 @@
 from datetime import date
+import os
 from django.db import models
 from django.contrib.postgres.fields import ArrayField
 from localflavor.es.forms import (ESPostalCodeField, ESProvinceSelect)
@@ -49,6 +50,93 @@ WEEKDAYS = [
   (7, ("Sunday")),
 ]
 
+def uploadOfficeFile(instance, filename):
+    
+    # Obtener el la referencia del inmueble
+    new_name = f"{instance.reference}"
+    # Obtener la extensión del archivo
+    _, extension = os.path.splitext(filename)
+    #Guardamos la variable de la carpeta donde meter el archivo
+    url = f"realEstate/officesPhotos/{new_name}/"
+    
+    #Si hay files en la carpeta, se ejecuta el bloque entero
+    try:
+        #Vemos los archvios de la carpeta
+        files = os.listdir(url)
+        #Creamos un nuevo array
+        newFiles = []
+
+        #Nos quedamos con el INT de cada foto y lo add al nuevo array
+        for file in files: 
+            number, _ = os.path.splitext(file)
+            newFiles.append(number)
+
+        #Ordenamos el array
+        newFiles.sort()
+
+        #Cogemos el ultimo elemento del array
+        lastFile = newFiles[-1]
+        contador = int(lastFile)
+        contador += 1
+        url = f"{url}/{contador}{extension}"
+
+    #En caso de que no haya files en la carpeta, se crea otra
+    except:
+        contador = 1
+        url = f"{url}/{contador}{extension}"
+
+    return url
+
+def uploadHouseFile(instance, filename):
+    
+    # Obtener el la referencia del inmueble
+    new_name = f"{instance.reference}"
+    # Obtener la extensión del archivo
+    _, extension = os.path.splitext(filename)
+    #Guardamos la variable de la carpeta donde meter el archivo
+    url = f"realEstate/housesPhotos/{new_name}/"
+    
+    #Si hay files en la carpeta, se ejecuta el bloque entero
+    try:
+        #Vemos los archvios de la carpeta
+        files = os.listdir(url)
+        #Creamos un nuevo array
+        newFiles = []
+
+        #Nos quedamos con el INT de cada foto y lo add al nuevo array
+        for file in files: 
+            number, _ = os.path.splitext(file)
+            newFiles.append(number)
+
+        #Ordenamos el array
+        newFiles.sort()
+
+        #Cogemos el ultimo elemento del array
+        lastFile = newFiles[-1]
+        contador = int(lastFile)
+        contador += 1
+        url = f"{url}/{contador}{extension}"
+
+    #En caso de que no haya files en la carpeta, se crea otra
+    except:
+        contador = 1
+        url = f"{url}/{contador}{extension}"
+
+    return url
+
+def agentFileName(instance, filename):
+    # Obtener el nombre y apellido del agente
+    new_name = f"{instance.fullName}"
+    
+    # Obtener la extensión del archivo
+    _, extension = os.path.splitext(filename)
+    url = f"realEstate/housesPhotos/{new_name}{extension}"
+    if os.path.exists(url):
+        os.remove(url)
+
+    # Devolver la ruta de destino completa
+    return f"realEstate/agentsPhoto/{new_name}{extension}"
+
 class OpeningHours(models.Model):
     weekday = models.IntegerField(choices=WEEKDAYS)
     from_hour = models.TimeField()
@@ -76,14 +164,18 @@ class Address(models.Model):
 
 class Office(models.Model):
     office = models.CharField(max_length=40)
+    reference = models.CharField(max_length=10, null=True, blank=True, unique=True)
     description = models.TextField()
     openingHours = models.ManyToManyField(OpeningHours)
     phone = models.CharField(max_length=9)
-    #fotos
     address = models.OneToOneField(Address, on_delete=models.CASCADE)
 
     def __str__(self):
         return f"{self.office}"
+    
+class OfficeImage(models.Model):
+    image = models.ImageField(upload_to=uploadOfficeFile)
+    office = models.ForeignKey(Office, on_delete=models.CASCADE)
 
 class Agent(models.Model):
     name = models.CharField(max_length=20)
@@ -93,10 +185,14 @@ class Agent(models.Model):
     email = models.EmailField()
     oficina = models.ForeignKey(Office, on_delete=models.CASCADE, null=True)
     idiomas = ArrayField(models.CharField(max_length=25), null=True)
-    photo = models.ImageField(upload_to="realEstate/agentsPhoto/", null=True)
+    photo = models.ImageField(upload_to=agentFileName, null=True)
     description = models.TextField()
     bestPhrase = models.CharField()
     linkRRSS = ArrayField(models.CharField(max_length=40))
+
+    @property
+    def fullName(self):
+        return f"{self.name.lower().replace(' ', '')}{self.lastname.lower().replace(' ', '')}"
 
     def __str__(self):
         return f"{self.name} {self.lastname} ({self.rol})"
@@ -123,8 +219,9 @@ class House(models.Model):
     country = models.CharField()
     agent = models.ForeignKey(Agent, on_delete=models.CASCADE, default=None)
 
-    url = "realEstate/housesPhotos/" + str(reference.name)
-    #photos = ArrayField(models.ImageField(upload_to=url), null=True)
-
     def __str__(self):
         return self.name
+    
+class HouseImage(models.Model):
+    image = models.ImageField(upload_to=uploadHouseFile)
+    house = models.ForeignKey(House, on_delete=models.CASCADE)
